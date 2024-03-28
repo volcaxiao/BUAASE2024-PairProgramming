850b6fad7ffd7f1142d4f4cc2fe41998c8e0d773
volca <volcaxiaoc@buaa.edu.cn>
2024-03-28 16:59:39 +0800

    T1: finish cpp file

---
+++ T1/t1_cpp/BocchiShutUp.cpp
@@ -0,0 +1,19 @@
+#include <array>
+#include <cstdint>
+#include <algorithm>
+
+int32_t bocchiShutUp(int32_t flag, int32_t seq[], int32_t size) {
+    std::array<int32_t, 7> cnt{};
+    int32_t flag10 = flag*10;
+    for (auto i = 0; i < size; ++i) {
+        int32_t index = seq[i] - flag10;
+        if (1 <= index && index <= 6) {
+            cnt[index]++;
+        }
+    }
+    auto it1 = std::max_element(cnt.begin(), cnt.end()) - cnt.begin();
+    auto it2 = std::max_element(cnt.rbegin(), cnt.rend()) - cnt.rbegin();
+    it2 = 6 - it2;
+    
+    return (it1==it2)? it1+flag10 : 10;
+}
