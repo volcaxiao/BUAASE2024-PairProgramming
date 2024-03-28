cc8ca4bfbb3722e3407ee23dac876c0cdc001e1e
volca <volcaxiaoc@buaa.edu.cn>
2024-03-28 18:50:12 +0800

    T1: todo bridge

--- T1/t1_cpp/BocchiShutUp.cpp
+++ T1/t1_cpp/BocchiShutUp.cpp
@@ -1,10 +1,14 @@
 #include <array>
+#include <cstdlib>
 #include <cstdint>
+#include <iostream>
 #include <algorithm>
 
-int32_t bocchiShutUp(int32_t flag, int32_t seq[], int32_t size) {
+extern "C"
+int64_t bocchiShutUp(int32_t flag, void * seq_a, int32_t size) {
     std::array<int32_t, 7> cnt{};
     int32_t flag10 = flag*10;
+    int32_t *seq = (int32_t*)seq_a;
     for (auto i = 0; i < size; ++i) {
         int32_t index = seq[i] - flag10;
         if (1 <= index && index <= 6) {
@@ -14,6 +18,15 @@ int32_t bocchiShutUp(int32_t flag, int32_t seq[], int32_t size) {
     auto it1 = std::max_element(cnt.begin(), cnt.end()) - cnt.begin();
     auto it2 = std::max_element(cnt.rbegin(), cnt.rend()) - cnt.rbegin();
     it2 = 6 - it2;
-    
-    return (it1==it2)? it1+flag10 : 10;
+    // std::cout << it1 << " " << it2 << std::endl;
+
+    // return (it1 == it2) ? it1 + flag10 : 10;
+    int64_t ans = 0;
+    for (auto i = 0; i < size; ++i) ans = ans * 100 + seq[i];
+    return ans;
 }
+
+// int main() {
+//     int32_t arr[] = {13,14,13,11,21,13,21,22};
+//     std::cout << bocchiShutUp(1, arr, 8) << std::endl;
+// }
